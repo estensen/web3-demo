48dbf255b46a8dd7f40fd85817e1ca21322b1442
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-01-16 09:46:22 +0100

    Replace address with account

--- 03_transaction.py
+++ 03_transaction.py
@@ -4,15 +4,15 @@ from web3 import Web3
 GANACHE_URL = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(GANACHE_URL))
 
-address_1 = "" # Fill in
-address_2 = "" # Fill in
+account_1 = "" # Fill in
+account_2 = "" # Fill in
 private_key = "" # Fill in
 
-nonce = web3.eth.getTransactionCount(address_1)
+nonce = web3.eth.getTransactionCount(account_1)
 
 tx = {
         "nonce": nonce,
-        "to": address_2,
+        "to": account_2,
         "value": web3.toWei(1, "ether"),
         "gas": 2000000,
         "gasPrice": web3.toWei("50", "gwei"),
