5265bdf7969bcc18c981d2aa6b702528b4bb881c
Håvard Anda Estensen <haavard.ae@gmail.com>
2020-01-16 09:38:57 +0100

    Use Ropsten

--- 01_blocknumber.py
+++ 01_blocknumber.py
@@ -7,8 +7,8 @@ if not (INFURA_API_KEY := os.getenv("INFURA_API_KEY")):
     print("INFURA_API_KEY env var is not set")
     sys.exit(0)
 
-# Mainnet is where the real money is
-INFURA_URL = f"https://mainnet.infura.io/v3/{INFURA_API_KEY}"
+# Ropsten is the test net that most resemble the main net
+INFURA_URL = f"https://ropsten.infura.io/v3/{INFURA_API_KEY}"
 
 web3 = Web3(Web3.HTTPProvider(INFURA_URL))
 
